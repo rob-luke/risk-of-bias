7447272191211d0fa9d37da73bf0c8ba3fc0a729
Robert Luke <code@robertluke.net>
2025-06-10 17:11:47 +1000

    chore: linting

--- risk_of_bias/frameworks/rob2/_rob2.py
+++ risk_of_bias/frameworks/rob2/_rob2.py
@@ -1,10 +1,7 @@
-from risk_of_bias.frameworks.rob2.domains._domain_1_randomization import \
-    domain_1_randomization
+import risk_of_bias.frameworks.rob2.domains._domain_1_randomization as d1
 from risk_of_bias.types._framework_types import Framework
 
 rob2_framework = Framework(
-    domains=[
-        domain_1_randomization,
-    ],
+    domains=[d1.domain_1_randomization],
     name="RoB2 Framework for Randomized Trials",
 )
